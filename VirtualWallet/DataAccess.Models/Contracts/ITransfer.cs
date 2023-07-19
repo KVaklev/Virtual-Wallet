3dd4bdba0015d4f19412c7ff64087dfb0ce7014e
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-07-19 16:01:57 +0300

    Added ITransfer interface and Transfer class

---
+++ VirtualWallet/DataAccess.Models/Contracts/ITransfer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Contracts
+{
+    public interface ITransfer
+    {
+        public int ID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal BalanceAmount { get; set; }
+
+        public int UserID { get; set; }
+
+        public int CurrencyID { get; set; }
+
+        public decimal DepositAmount { get; set; }
+
+        public decimal WithdrawalAmount { get; set; }
+    }
+}
