f5ce793a8e98c00d6dc858a1cfe8aa09923bfc70
Marina Toncheva <114232957+MarinaToncheva@users.noreply.github.com>
2023-07-19 16:34:39 +0300

    Added Card and EmailVerification classes, enum CardType

--- VirtualWallet/DataAccess.Models/Models/User.cs
+++ VirtualWallet/DataAccess.Models/Models/User.cs
@@ -5,6 +5,8 @@ namespace DataAccess.Models.Models
 {
     public class User : IUser
     {
+        public int Id { get; set; }
+
         [MinLength(2, ErrorMessage = "The {0} must be at least {1} characters long.")]
         [MaxLength(32, ErrorMessage = "The {0} must be no more than {1} characters long.")]
         public string FirstName { get; set; }
