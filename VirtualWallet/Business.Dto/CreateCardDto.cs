05fc741b3915a87bb0f9f2d81bf749750bea539c
Marina Toncheva <mar.toncheva@gmail.com>
2023-07-26 16:14:57 +0300

    Added CreateCardDto, Fixed validation attributes with Constants class

--- VirtualWallet/Business.Dto/CreateCardDto.cs
+++ VirtualWallet/Business.Dto/CreateCardDto.cs
@@ -1,7 +1,42 @@
-﻿namespace Business.Dto
+﻿using DataAccess.Models.Enums;
+using DataAccess.Models.Models;
+using DataAccess.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Business.Dto
 {
     public class CreateCardDto
     {
+
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        [CardNumber(ErrorMessage = Constants.CardNumberFieldErroMessage)]
+        [StringLength(16, ErrorMessage = Constants.CardNumberLengthErrorMessage)]
+        public string CardNumber { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        public DateTime ExpirationDate { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        [MinLength(Constants.CardHolderMinLength, ErrorMessage = Constants.CardHolderMinLengthErrorMessage)]
+        [MaxLength(Constants.CardHolderMaxLength, ErrorMessage = Constants.CardHolderMaxLengthErrorMessage)]
+        public string CardHolder { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        [CardCheckNumber(ErrorMessage = Constants.CardCheckNumberFieldErrorMessage)]
+        [StringLength(Constants.CheckNumberLength, ErrorMessage = Constants.CheckNumberLengthErrorMessage)]
+        public string CheckNumber { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        public CardType CardType { get; set; }
+        public int AccountId { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        public decimal? Balance { get; set; }
+        public decimal? CreditLimit { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        public Currency? Currency { get; set; }
 
     }
 }
--- VirtualWallet/DataAccess.Models/Models/Card.cs
+++ VirtualWallet/DataAccess.Models/Models/Card.cs
@@ -8,29 +8,34 @@ namespace DataAccess.Models.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "The {0} field is required")]
-        [CardNumber(ErrorMessage = "The card number must contain only digits.")]
-        [StringLength(16, ErrorMessage = "The {0} must be exactly {1} characters long.")]
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        [CardNumber(ErrorMessage = Constants.CardNumberFieldErroMessage)]
+        [StringLength(16, ErrorMessage = Constants.CardNumberLengthErrorMessage)]
         public string CardNumber { get; set; }
 
-        [Required(ErrorMessage = "The {0} field is required")]
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
         public DateTime ExpirationDate { get; set; }
 
-        [Required(ErrorMessage = "The {0} field is required")]
-        [MinLength(2, ErrorMessage = "The {0} must be at least {1} characters long.")]
-        [MaxLength(30, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        [MinLength(Constants.CardHolderMinLength, ErrorMessage = Constants.CardHolderMinLengthErrorMessage)]
+        [MaxLength(Constants.CardHolderMaxLength, ErrorMessage = Constants.CardHolderMaxLengthErrorMessage)]
         public string CardHolder { get; set; }
 
-        [Required(ErrorMessage = "The {0} field is required")]
-        [CardCheckNumber(ErrorMessage = "The card check number must contain only digits.")]
-        [StringLength(3, ErrorMessage = "The {0} must be exactly {1} characters long.")]
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
+        [CardCheckNumber(ErrorMessage = Constants.CardCheckNumberFieldErrorMessage)]
+        [StringLength(Constants.CheckNumberLength, ErrorMessage = Constants.CheckNumberLengthErrorMessage)]
         public string CheckNumber { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
         public CardType CardType { get; set; }
         public int AccountId { get; set; }
         public Account Account { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
         public decimal? Balance { get; set; }
         public decimal? CreditLimit { get; set; }
 
+        [Required(ErrorMessage = Constants.EmptyFieldErrorMessage)]
         public Currency? Currency { get; set; }
 
         public int? CurrencyId { get; set; }
