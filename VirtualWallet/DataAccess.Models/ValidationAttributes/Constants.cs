ca0dfdefa672a1166178812a9d118441193c86ee
Evelina Hristova <ev.hristova@gmail.com>
2023-08-02 14:41:59 +0300

    Edited Messages in Constants.

--- VirtualWallet/DataAccess.Models/ValidationAttributes/Constants.cs
+++ VirtualWallet/DataAccess.Models/ValidationAttributes/Constants.cs
@@ -33,8 +33,23 @@
         public const int CheckNumberLength = 3;
         public const string CheckNumberLengthErrorMessage = "The {0} must be exactly {1} characters long.";
 
-        //Others
-        public const string EmptyFieldErrorMessage = "The field is required.";
         public const string EmptyFieldCardTypeErrorMessage = "The field is required. Specify 'Debit' or 'Credit' card ";
+
+        //Constants for currency
+        public const int CurrencyNameMinLength = 2;
+        public const int CurrencyNameMaxLength = 30;
+        public const int CurrencyCodeLength = 3;
+
+
+        //Common
+        public const int IdMinLength = 1;
+        public const int IdMaxLength = int.MaxValue;
+
+        public const string NoFoundErrorMessage = "No matches found.";
+        public const string EmptyFieldErrorMessage = "The field is required.";
+        public const string RangeFieldErrorMessage = "The {0} field must be in the range from {1} to {2}.";
+        public const string MinLengthErrorMessage = "The {0} must be at least {1} characters long.";
+        public const string MaxLengthErrorMessage = "The {0} must be no more than {1} characters long.";
+        public const string LengthErrorMessage = "The {0} must be exactly {1} characters long.";
     }
 }
