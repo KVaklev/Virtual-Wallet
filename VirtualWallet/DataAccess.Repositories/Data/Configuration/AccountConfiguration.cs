f74c52a7041516e804310b9c68f0b2172cd26af6
Marina Toncheva <mar.toncheva@gmail.com>
2023-08-02 10:36:45 +0300

    Refactor DbContext with Configuration classes and ModelBuilderExtension/ (Fluent API)

---
+++ VirtualWallet/DataAccess.Repositories/Data/Configuration/AccountConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Repositories.Data.Configuration
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Card>
+    {
+        public void Configure(EntityTypeBuilder<Card> builder)
+        {
+
+        }
+    }
+}
---
+++ VirtualWallet/DataAccess.Repositories/Data/Configuration/CurrencyConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Repositories.Data.Configuration
+{
+    public class CurrencyConfiguration : IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder
+               .HasIndex(b => b.Name)
+               .IsUnique();
+        }
+    }
+}
