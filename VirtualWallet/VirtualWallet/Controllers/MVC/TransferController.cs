a0c09955f08ed44161721d142adb5c3ca94bc59f
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-08-05 18:16:35 +0300

    Transfer MVC Controller started

--- VirtualWallet/VirtualWallet/Controllers/MVC/TransferController.cs
+++ VirtualWallet/VirtualWallet/Controllers/MVC/TransferController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.QueryParameters;
+using Business.Services.Contracts;
+using DataAccess.Models.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace VirtualWallet.Controllers.MVC
 {
     public class TransferController : Controller
     {
-        public IActionResult Index()
+        private readonly ITransferService transferService;
+
+        public TransferController(ITransferService transferService)
         {
+            this.transferService = transferService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] TransferQueryParameters transferQueryParameters)
+        {
+            if (!this.HttpContext.Session.Keys.Contains("LoggedUser"))
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
             return View();
         }
     }
