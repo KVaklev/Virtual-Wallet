54ebda6a8c21be0a0eaeb77cd877a90c219359f0
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-08-15 11:23:37 +0300

    CreateTransferViewModel added

---
+++ VirtualWallet/Business.ViewModels/CreateTransferViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Business.DTOs.Requests;
+using Business.DTOs.Responses;
+using DataAccess.Models.Enums;
+using DataAccess.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ViewModels
+{
+    public class CreateTransferViewModel
+    {
+        public CreateTransferDto CreateTransferDto { get; set; }
+
+       // public User User { get; set; }
+
+        public List<GetCreatedCardDto> Cards { get; set; }
+
+        public string CurrencyCode { get; set; }
+
+        public TransferDirection TransferDirection { get; set; }
+    }
+}
