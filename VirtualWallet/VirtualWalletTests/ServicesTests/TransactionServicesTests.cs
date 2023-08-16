d4ae96c139bc06929869bd99ef23a742059f2a5f
Marina Toncheva <mar.toncheva@gmail.com>
2023-08-16 16:37:02 +0300

    Fix

--- VirtualWallet/VirtualWalletTests/ServicesTests/TransactionServicesTests.cs
+++ VirtualWallet/VirtualWalletTests/ServicesTests/TransactionServicesTests.cs
@@ -1,19 +1,18 @@
-﻿namespace VirtualWalletTests.ServicesTests
-{
-    using AutoMapper;
-    using Business.DTOs.Responses;
-    using Business.Services.Contracts;
-    using Business.Services.Helpers;
-    using Business.Services.Models;
-    using DataAccess.Models.Models;
-    using DataAccess.Repositories.Contracts;
-    using Moq; // You may need to install this package for mocking dependencies
+﻿using AutoMapper;
+using Business.DTOs.Responses;
+using Business.Mappers;
+using Business.Services.Contracts;
+using Business.Services.Helpers;
+using Business.Services.Models;
+using DataAccess.Models.Models;
+using DataAccess.Repositories.Contracts;
+using Moq;
 
+namespace VirtualWalletTests.ServicesTests
+{
     [TestClass]
     public class TransactionServiceTests
     {
        
-            // Similar tests can be written for other scenarios (invalid transaction, failed checks, etc.)
-        
+        }
     }
-}
